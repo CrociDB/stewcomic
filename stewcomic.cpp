3cf4d8da093d775b9ca708867fcefa01b2615a26
Bruno Croci <crocidb@gmail.com>
2013-10-13 01:40:18 -0300

    basic tabbed comics support

--- stewcomic.cpp
+++ stewcomic.cpp
@@ -1,11 +1,14 @@
 #include "stewcomic.h"
 #include "ui_stewcomic.h"
+#include "viewerwidget.h"
 
 stewcomic::stewcomic(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::stewcomic)
 {
     ui->setupUi(this);
+
+    ui->tabWidget->clear();
 }
 
 stewcomic::~stewcomic()
@@ -23,5 +26,7 @@ void stewcomic::on_actionOpen_triggered()
     FileExtractor fe(fileName, dir_tool.dir());
     fe.extractFile();
 
-    ui->widget->openComic(Comic(fe.getDir()));
+    ViewerWidget *view = new ViewerWidget(ui->tabWidget);
+    view->openComic(Comic(fe.getDir()));
+    ui->tabWidget->addTab(view, fileName.mid(fileName.lastIndexOf("/")+1, fileName.length()).mid(0, 20));
 }
